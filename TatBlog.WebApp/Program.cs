f410cb3005a52eb34be46da8fefc9dbdc0e768b9
Hoang21002 <dinhhoang026@gmail.com>
2023-03-07 20:30:30 +0700

    Xong phan 6B lab2

--- TatBlog.WebApp/Program.cs
+++ TatBlog.WebApp/Program.cs
@@ -1,6 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Net;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+{
+    /*Them cac dich vu duoc yeu cau boi MVC Framework*/
+    builder.Services.AddControllersWithViews();
+}
+var app = builder.Build();
+{
+    /*Cau hinh HTTP Request pipeline*/
+
+    /*Them middleware de hien thi thong bao loi*/
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Blog/Error");
+        /*Them middleware cho viec ap dung HSTS (them header
+            Strict-TransportContext-Security vao HTTP Response)*/
+
+        app.UseHsts();
+    }
+
+    /*Them middleware de chuyen huong HTTP sang HTTPS*/
+    app.UseHttpsRedirection();
+
+    /*Them middleware phuc vu cac yeu cau lien quan toi cac
+        tap tin noi dung tinh nhu hinh anh, css,...*/
+    app.UseStaticFiles();
+
+    /*Them middleware lua chon endpoint phu hop nhat de 
+        xu ly mot HTTP request*/
+    app.UseRouting();
+
+    /*Dinh nghia route template, route constraint cho cac endpoints
+        ket hop voi cac action trong cac controller*/
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
+
+/*app.MapGet("/", () => "Hello World!");*/
 
 app.Run();
