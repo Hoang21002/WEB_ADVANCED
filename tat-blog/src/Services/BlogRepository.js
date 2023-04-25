c4dc8bc00d4405a114303cbb071720e33b452114
Hoang21002 <dinhhoang026@gmail.com>
2023-04-25 22:59:31 +0700

    Xong lab6

--- tat-blog/src/Services/BlogRepository.js
+++ tat-blog/src/Services/BlogRepository.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { get_api, post_api } from './Methods';
+
 
 export async function getPosts(keyword = '', pageSize = 10, pageNumber = 1,
-sortColumn = '', sortOrder = '') {
+    sortColumn = '', sortOrder = '') {
     try {
-        const response = await 
-axios.get(`http://localhost:7085/api/posts?keyword=${keyword}&PageSize=${
-    pageSize}&PageNumber=${pageNumber}&SortColumn=${sortColumn}&SortOrder=${
-        sortOrder}`);
+        const response = await
+            axios.get(`http://localhost:7085/api/posts?keyword=${keyword}&PageSize=${pageSize}&PageNumber=${pageNumber}&SortColumn=${sortColumn}&SortOrder=${sortOrder}`);
         const data = response.data;
         if (data.isSuccess)
             return data.result;
@@ -15,6 +15,31 @@ axios.get(`http://localhost:7085/api/posts?keyword=${keyword}&PageSize=${
     } catch (error) {
         console.log('Error', error.message);
         return null;
-    }    
+    }
+}
+
+export async function getPostById(id = 0) {
+    if (id > 0)
+        return get_api(`https://localhost:7085/api/posts/${id}`);
+    return null;
+}
+export function addOrUpdatePost(formData) {
+    return post_api('https://localhost:7085/api/posts', formData);
+}
+
+export function getPostsFilter(keyword = '', authorId = '', categoryId = '',
+    year = '', month = '', pageSize = 10, pageNumber = 1, sortColumn = '',
+    sortOrder = '') {
+    let url = new URL('https://localhost:7085/api/posts/get-posts-filter');
+    keyword !== '' && url.searchParams.append('Keyword', keyword);
+    authorId !== '' && url.searchParams.append('AuthorId', authorId);
+    categoryId !== '' && url.searchParams.append('CategoryId', categoryId);
+    year !== '' && url.searchParams.append('Year', year);
+    month !== '' && url.searchParams.append('Month', month);
+    sortColumn !== '' && url.searchParams.append('SortColumn', sortColumn);
+    sortOrder !== '' && url.searchParams.append('SortOrder', sortOrder);
+    url.searchParams.append('PageSize', pageSize);
+    url.searchParams.append('PageNumber', pageNumber);
+    return get_api(url.href);
 }
 
